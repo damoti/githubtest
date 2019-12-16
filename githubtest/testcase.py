b2a8d04450228b2634f8ef9004f0b7d03ca1f018
Lex Berezhny <lex@damoti.com>
2019-12-15 21:37:10 -0500

    tunnel debugging

--- githubtest/testcase.py
+++ githubtest/testcase.py
@@ -14,12 +14,14 @@ class GitHubTestCase(IsolatedAsyncioTestCase):
 
     @classmethod
     def setUpClass(cls):
+        print('before tunnel')
         if 'TEST_USE_LOCALTUNNEL' in os.environ:
             tunnel = Localtunnel.sync_start(os.environ['TEST_USE_LOCALTUNNEL'], cls.APP_PORT)
             cls.addClassCleanup(tunnel.sync_stop)
             webhook = tunnel.url
         else:
             webhook = f"http://{get_external_ip()}:{cls.APP_PORT}"
+        print('after tunnel')
 
         cls.APP_MANIFEST["name"] += f" {urlparse(webhook).netloc}"
         if len(cls.APP_MANIFEST["name"]) > 34:  # GitHub does not allow names longer than 34.
