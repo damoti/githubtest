70de1d4d3fba90cbd0fc1020b5e38bd70c702cc7
Lex Berezhny <lex@damoti.com>
2019-12-15 19:56:18 -0500

    some output

--- githubtest/user.py
+++ githubtest/user.py
@@ -52,7 +52,9 @@ class GitHubTestUser:
     @classmethod
     def connect(cls, manifest, secret, oauth, username, password) -> 'GitHub':
         gh = cls(manifest, requests.Session())
+        print('gh.login')
         gh.login(secret, username, password)
+        print('gh._user.login')
         gh._user.login(token=oauth)
         return gh
 
